d7ad71c9aa9cd6ba3c64c5a0cf5afbd4e41d67f8
Érico Meger <86668447+EricoMeger@users.noreply.github.com>
2023-06-18 17:32:52 -0300

    created new AI difficulty choice screen

--- janelas.py
+++ janelas.py
@@ -16,6 +16,19 @@ class Screens():
 
         return a
 
+    def IAChoiceScreen(self):
+        print("==============================================")
+        print("|                 VOCÊ DESEJA...             |")
+        print("==============================================")
+        print("|              1 - Modo fácil                |")
+        print("|(De vez em quando a IA fará jogadas erradas)|")
+        print("|              2 - Modo difícil              |")
+        print("|   (Você só conseguirá perder ou empatar)   |")
+        print("==============================================")
+        a = input("Escolha uma opção: ")
+        
+        return a
+
     def victoryScreen(self, jogador):
         print("\n         === JOGO DA VELHA ===")
         print("\n==-==-==-==-==-==-==-==-==-==-==-==-==")
